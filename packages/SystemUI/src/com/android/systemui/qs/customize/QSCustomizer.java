a41c75088e38579e79a10e6b674ce85d7f259f1e
althafvly <althafvly@gmail.com>
2022-09-28 21:06:43 +0530

    SystemUI: QSCustomizer: Add reset to toolbar menu
    Change-Id: I49a599f10f68c0c576ec863b919936a3b171f2c6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -22,6 +22,9 @@ import android.animation.Animator.AnimatorListener;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -83,7 +86,11 @@ public class QSCustomizer extends LinearLayout {
         mToolbar.setNavigationIcon(
                 getResources().getDrawable(value.resourceId, themedContext.getTheme()));
 
-        mToolbar.getMenu().add(Menu.NONE, MENU_RESET, 0, com.android.internal.R.string.reset)
+        SpannableString resetText = new SpannableString(
+                mContext.getString(com.android.internal.R.string.reset));
+        resetText.setSpan(new ForegroundColorSpan(isNightMode() ?
+                Color.WHITE : Color.BLACK), 0, resetText.length(), 0);
+        mToolbar.getMenu().add(Menu.NONE, MENU_RESET, 0, resetText)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
         mToolbar.setTitle(R.string.qs_edit);
         if (gsfQuickSettings()) {
@@ -113,6 +120,11 @@ public class QSCustomizer extends LinearLayout {
             mSceneContainerEnabled = enabled;
             updateTransparentViewHeight();
         }
+    }
+
+    private boolean isNightMode() {
+        return (mContext.getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
     }
 
     void updateResources() {
@@ -303,9 +315,13 @@ public class QSCustomizer extends LinearLayout {
     }
 
     private void updateToolbarMenuFontSize() {
+        SpannableString resetText = new SpannableString(
+                mContext.getString(com.android.internal.R.string.reset));
+        resetText.setSpan(new ForegroundColorSpan(isNightMode() ?
+                Color.WHITE : Color.BLACK), 0, resetText.length(), 0);
         // Clearing and re-adding the toolbar action force updates the font size
         mToolbar.getMenu().clear();
-        mToolbar.getMenu().add(Menu.NONE, MENU_RESET, 0, com.android.internal.R.string.reset)
+        mToolbar.getMenu().add(Menu.NONE, MENU_RESET, 0, resetText)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
     }
 }
